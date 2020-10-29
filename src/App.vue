75be145672184b267477ffbb54e5e05c396c2921
ikenoue-ryo <ikenoue.ryo@gmail.com>
2020-10-29 10:06:25 +0900

    1st

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <input type="text" v-model="address">
+    <button type="button" @click="mapSearch">検索</button>
+    <div id="map"></div>
   </div>
 </template>
 
@@ -10,9 +11,37 @@
 
 <script>
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'Map',
+  data() {
+    return {
+      map: {},
+      marker: null,
+      geocoder: {},
+      address: '福岡県福岡市博多区'
+    }
+  },
+  mounted() {
+    this.map = new google.maps.Map(document.getElementById('map'));
+    this.geocoder = new google.maps.Geocoder();
+  },
+  methods: {
+    mapSearch() {
+      this.geocoder.geocode({
+        'address': this.address,
+        newForwardGeocoder:true
+      }, (results, status) => {
+        if (status === google.maps.GeocoderStatus.OK) {
+          this.map.setCenter(results[0].geometry.location);
+          // 緯度経度の取得
+          // results[0].geometry.location.lat();
+          // results[0].geometry.location.lng();
+          this.marker = new google.maps.Marker({
+            map: this.map,
+            position: results[0].geometry.location
+          });
+        }
+      });
+    }
   }
 }
 </script>
