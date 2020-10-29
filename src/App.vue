13c4bcd7b6d8edd586af7bc31d28c5c98c395fa6
ikenoue-ryo <ikenoue.ryo@gmail.com>
2020-10-29 10:01:26 +0900

    4th

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <div class="map" ref="googleMap"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
@@ -9,45 +10,9 @@
 
 <script>
 export default {
-  name: 'Map',
-  data() {
-    return {
-      google: null,
-      comments: null,
-      mapConfig: {
-        center: {
-          lat: null,
-          lng: null
-        },
-        zoom: 17
-      },
-      lat: '',
-      lng: '',
-    }
-  },
-  async mounted() {
-    const URL = 'https://maps.googleapis.com/maps/api/geocode/json?new_forward_geocoder=true&address='
-    const gapiKey = '&key=' + 'ここにAPIキー'
-    const search_address = '福岡県福岡市中央区天神' 
-
-    this.mapConfig.center.lat = 33.6000737 //①
-    this.mapConfig.center.lng = 130.4279274 //②
-
-    axios.get(URL + search_address + gapiKey)
-    .then(response => { 
-      console.log(response) //responseのデータを①と②に渡したい。 JSON：response.data.results[0].geometry.bounds.northeast.lng
-      })
-    .catch(response => console.log(response))
-
-    this.google = await GoogleMapsApiLoader({
-      apiKey: 'ここにAPIキー'
-    });
-    this.initializeMap();
-  },
-  methods: {
-    initializeMap() {
-      new this.google.maps.Map(this.$refs.googleMap, this.mapConfig);
-    }
+  name: 'App',
+  components: {
+    HelloWorld
   }
 }
 </script>
